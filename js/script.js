b22dd7234b802b0bb4bec3c8c6c9d5d1613b6b7e
work <languagelearning@work-2.local>
2017-07-17 16:37:13 -0600

    2.2

--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
 // Array of Quotes
 var quotes = [
   {
-    quote:  'I am the greatest, I said that even before I knew I was.',
-    source: 'Muhammad Ali',
-    citation: 'foo',
-    year: 'bar'
-  },
-  {
-    quote:  'The future belongs to those who prepare for it today.',
-    source: 'Malcolm X',
-    citation: 'foo',
-    year: ''
-  },
-  {
-    quote:  'Work is love made visible.',
-    source: 'Kahlil Gibran',
-    citation: 'The Prophet',
-    year: '1923'
-  },
-  {
-    quote:  'You don't like something, change it. If you can't change it, change your attitude.',
-    source: 'Maya Angelou',
-    citation: '',
-    year: ''
-  },
-  {
-      quote:  'ahkbar',
-      source: 'Muhammad ',
-      citation: 'ala',
-      year: 'bar'
+      quote:  'I am the greatest!',
+      source: 'Muhammad Ali',
+      citation: 'Liston vs Ali',
+      year: '1964',
+      color: 'darkslategrey',
+      img: 'm1.jpg'
+    },
+    {
+      quote:  ' Brotherhood is a two-way street.[.',
+      source: 'Malcolm X',
+      citation: 'Speech at Harvard Forum',
+      year: '1964',
+      color: 'darkblue',
+      img: 'mx1.jpg'
+    },
+    {
+      quote:  'Work is love made visible.',
+      source: 'Kahlil Gibran',
+      citation: 'The Prophet',
+      year: '1923',
+      color: 'cyan',
+      img: 'theProphet.jpg'
+    },
+    {
+      quote:  'I don\'t trust people who don\'t love themselves and tell me "I love you."',
+      source: 'Maya Angelou',
+      citation: 'The Distinguished Annie Clark Tanner Lecture',
+      year: '1997',
+      color: 'darkred',
+      img: 'maya.jpg'
+    },
+    {
+        quote:  'All types of knowledge, ultimately mean self knowledge.',
+        source: 'Bruce Lee',
+        citation: 'Bruce Lee: The Lost Interview',
+        year: '1971',
+        color: 'gold',
+        img: 'brucelee.jpg'
   }
 ];
 var shownQuotes = [];
@@ -39,28 +49,47 @@ var quoteElement = box.querySelectorAll('.quote')[0];
 var sourceElement = box.querySelectorAll('.source')[0];
 var citationElement = box.querySelectorAll('.citation')[0];
 var yearElement = box.querySelectorAll('.year')[0];
+var imgBox = document.getElementById('img-box');
+var quoteButton = document.getElementById('loadQuote')
 
-// function that randomizes the quotes by using math.random mulitplied by the of the array then floored to give us a random quote.
+// function that randomizes the quotes by using math.random mulitplied by the length of the array (5) then floored to give us a random quote.
 function getRandomIndex() {
   return Math.floor(Math.random() * quotes.length)
-  if (shownQuotes.length === quotes.length){
-      shownQuotes.length = 0;
-  }
 }
 //function that prints the quote
-
 function printQuote () {
-  var randomIndex;
-  do {
-      randomIndex = getRandomIndex();
-  } while (shownQuotes.indexOf(randomIndex) !== -1)
-  var randomQuote = quotes[randomIndex];
+  var randomIndex = getRandomIndex();
+  //starts the page with a random quote on page load
+  var randomQuote = quotes.splice(randomIndex, 1)[0];
+  //loads the quote text
   quoteElement.innerText = randomQuote.quote;
-  sourceElement.innerText = randomQuote.source;
+  //loads the string value from the array defined in var quotes
   citationElement.innerText = randomQuote.citation;
+  //loads the year string from the quotes array
   yearElement.innerText = randomQuote.year;
-  shownQuotes.push(randomIndex);
+  //loads the source of the quote
+  sourceElement.innerText = randomQuote.source;
+  if(citationElement != '1'){
+    sourceElement.appendChild(yearElement);
+    sourceElement.appendChild(citationElement);
+  }
+  //loads the picture into the div .img-box
+  imgBox.style.backgroundImage = 'url(./img/' + randomQuote.img + ')';
+  //loads the color associated with quotes
+  document.body.style.backgroundColor= randomQuote.color;
+  //uses pushes quote seen on screen to array shownQuotes
+  shownQuotes.push(randomQuote);
+  //colors the button to match the background color of the quote shown
+  quoteButton.style.backgroundColor = randomQuote.color;
+  //ensures that all quotes are shown before showing them again
+  // if quotes array
+  if (quotes.length === 0){
+    quotes = shownQuotes;
+    shownQuotes = [];
+  }
 }
+printQuote();
+
 
 
 // event listener to respond to "Show another quote" button clicks
