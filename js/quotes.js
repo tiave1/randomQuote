fa12cd3e0529ef30282389d476c4b7cde3ea1116
Adam Salima <tiave.s@gmail.com>
2017-07-26 21:44:07 -0600

    '2.21'

---
+++ js/quotes.js
@@ -0,0 +1,48 @@
+// Array of Quotes
+var quotes = [
+  {
+      quote:  'I am the greatest!',
+      source: 'Muhammad Ali',
+      citation: '',
+      year: '',
+      color: 'darkslategrey',
+      img: 'm1.jpg',
+      tag: ["Boxing", "Ali"]
+    },
+    {
+      quote:  ' Brotherhood is a two-way street.',
+      source: 'Malcolm X',
+      citation: 'Speech at Harvard Forum',
+      year: '1964',
+      color: 'darkblue',
+      img: 'mx1.jpg',
+      tag: ["Malcolm", "Civil Rights"]
+    },
+    {
+      quote:  'Work is love made visible.',
+      source: 'Kahlil Gibran',
+      citation: 'The Prophet',
+      year: '1923',
+      color: 'cyan',
+      img: 'theProphet.jpg',
+      tag: ["Gibran", "Literture"]
+    },
+    {
+      quote:  'I don\'t trust people who don\'t love themselves and tell me "I love you."',
+      source: 'Maya Angelou',
+      citation: 'The Distinguished Annie Clark Tanner Lecture',
+      year: '1997',
+      color: 'darkred',
+      img: 'maya.jpg',
+      tag: ["Civil Rights", "Weber State University"]
+    },
+    {
+        quote:  'All types of knowledge, ultimately mean self knowledge.',
+        source: 'Bruce Lee',
+        citation: 'Bruce Lee: The Lost Interview',
+        year: '1971',
+        color: 'gold',
+        img: 'brucelee.jpg',
+        tag:["The Dragon","Martial Arts"]
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,46 +1,4 @@
-// Array of Quotes
-var quotes = [
-  {
-      quote:  'I am the greatest!',
-      source: 'Muhammad Ali',
-      citation: 'Liston vs Ali',
-      year: '1964',
-      color: 'darkslategrey',
-      img: 'm1.jpg'
-    },
-    {
-      quote:  ' Brotherhood is a two-way street.[.',
-      source: 'Malcolm X',
-      citation: 'Speech at Harvard Forum',
-      year: '1964',
-      color: 'darkblue',
-      img: 'mx1.jpg'
-    },
-    {
-      quote:  'Work is love made visible.',
-      source: 'Kahlil Gibran',
-      citation: 'The Prophet',
-      year: '1923',
-      color: 'cyan',
-      img: 'theProphet.jpg'
-    },
-    {
-      quote:  'I don\'t trust people who don\'t love themselves and tell me "I love you."',
-      source: 'Maya Angelou',
-      citation: 'The Distinguished Annie Clark Tanner Lecture',
-      year: '1997',
-      color: 'darkred',
-      img: 'maya.jpg'
-    },
-    {
-        quote:  'All types of knowledge, ultimately mean self knowledge.',
-        source: 'Bruce Lee',
-        citation: 'Bruce Lee: The Lost Interview',
-        year: '1971',
-        color: 'gold',
-        img: 'brucelee.jpg'
-  }
-];
+
 var shownQuotes = [];
 
 // variables
@@ -52,15 +10,28 @@ var yearElement = box.querySelectorAll('.year')[0];
 var imgBox = document.getElementById('img-box');
 var quoteButton = document.getElementById('loadQuote')
 
+//random color function that returns hex value.
+function getRandomColor() {
+    return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+}
+
+//Refreshes quote in three seconds
+function refreshQuote(refreshRate) {
+    return window.setInterval(printQuote, refreshRate);
+}
+
 // function that randomizes the quotes by using math.random mulitplied by the length of the array (5) then floored to give us a random quote.
 function getRandomIndex() {
   return Math.floor(Math.random() * quotes.length)
 }
 //function that prints the quote
 function printQuote () {
+  //prints random quote on click
   var randomIndex = getRandomIndex();
   //starts the page with a random quote on page load
   var randomQuote = quotes.splice(randomIndex, 1)[0];
+  var randomColor = getRandomColor();
+  //loads random color function
   //loads the quote text
   quoteElement.innerText = randomQuote.quote;
   //loads the string value from the array defined in var quotes
@@ -69,24 +40,36 @@ function printQuote () {
   yearElement.innerText = randomQuote.year;
   //loads the source of the quote
   sourceElement.innerText = randomQuote.source;
-  if(citationElement != '1'){
-    sourceElement.appendChild(yearElement);
+
+//checks if citation property has a value if not it doesn't append the span
+  if (citationElement.innerText !== '') {
     sourceElement.appendChild(citationElement);
+  } else {
+
+  }
+//checks if year property has a value if not it doesn't append the span
+  if (yearElement.innerText !== '') {
+    sourceElement.appendChild(yearElement);
+  }else {
+
   }
   //loads the picture into the div .img-box
   imgBox.style.backgroundImage = 'url(./img/' + randomQuote.img + ')';
-  //loads the color associated with quotes
-  document.body.style.backgroundColor= randomQuote.color;
+
   //uses pushes quote seen on screen to array shownQuotes
   shownQuotes.push(randomQuote);
   //colors the button to match the background color of the quote shown
-  quoteButton.style.backgroundColor = randomQuote.color;
+  quoteButton.style.backgroundColor = randomColor;
   //ensures that all quotes are shown before showing them again
   // if quotes array
   if (quotes.length === 0){
     quotes = shownQuotes;
     shownQuotes = [];
   }
+  // Change body Background Color
+   document.body.style.background = randomColor;
+   //colors the button to match the background color of the quote shown
+   quoteButton.style.backgroundColor = randomColor;
 }
 printQuote();
 
@@ -94,4 +77,4 @@ printQuote();
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote,  refreshQuote(10000), false);
